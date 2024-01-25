6d0a885d24b426a0deb2fd6fa13c046574c1d331
RAragonGomez <renalf14@gmail.com>
2024-01-25 14:34:57 -0500

    tarea

--- M05 Fundamentos Javascript/ejercicios/10.js
+++ M05 Fundamentos Javascript/ejercicios/10.js
@@ -3,6 +3,13 @@ function esFechaValida(fecha) {
   // Comprueba si este corresponde a una fecha válida.
   // Si es así, retorna true, sino retorna false.
   // Tu código:
+  if(fecha instanceof Date){
+    const tiempoEnMilisegundos = fecha.getTime() ;
+  if (!isNaN(tiempoEnMilisegundos)) {
+  return true;
+}
+} 
+  return false;
 }
 
 module.exports = esFechaValida;
--- M05 Fundamentos Javascript/ejercicios/11.js
+++ M05 Fundamentos Javascript/ejercicios/11.js
@@ -5,6 +5,13 @@ function esMayorDeEdad(fechaNacimiento) {
   // Devuelve true si lo es, caso contrario, retorna false.
   // PISTA: Investiga el método getFullYear() de la clase Date para obtener el año actual.
   // Tu código:
+  const fechaNac = new Date(fechaNacimiento);
+
+  const fechaActual = new Date();
+
+  const edad = fechaActual.getFullYear() - fechaNac.getFullYear();
+
+  return edad >= 18;
 }
 
 module.exports = esMayorDeEdad;
